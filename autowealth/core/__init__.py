45ec1073f259f25168f0d8e3dbd2b87308def1fb
Jsoned <443684353@qq.com>
2026-06-04 21:55:50 +0800

    refactor: 添加公共API导出

---
+++ autowealth/core/__init__.py
@@ -0,0 +1,12 @@
+from autowealth.core.engine import AutoWealthEngine, quick_analyze, batch_analyze
+from autowealth.core.analyzer import TechnicalAnalyzer, FundamentalAnalyzer
+from autowealth.core.data_fetcher import DataFetcher
+
+__all__ = [
+    "AutoWealthEngine",
+    "quick_analyze",
+    "batch_analyze",
+    "TechnicalAnalyzer",
+    "FundamentalAnalyzer",
+    "DataFetcher",
+]
